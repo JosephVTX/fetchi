5b6301d3d37bdbb06c2b9c3caeecd288a4cf5fa9
JosephVTX <jvcallupe69@gmail.com>
2024-05-20 20:54:27 -0500

    ....

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const chromium = require("@sparticuz/chromium");
-const { chromium: playwright } = require("playwright-core");
+const puppeteer = require("puppeteer-core");
 const app = require("express")();
 
 app.get("/", async (_, res) => {
-  const browser = await playwright.launch({
+  const browser = await puppeteer.launch({
     args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
     executablePath: await chromium.executablePath(),
     headless: chromium.headless,
   });
+
   const page = await browser.newPage();
   await page.goto("https://example.com");
   const title = await page.title();
