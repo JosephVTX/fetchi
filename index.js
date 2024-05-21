b85bc0b88aae4b7620065a83c6bf4f84b5e29da1
JosephVTX <jvcallupe69@gmail.com>
2024-05-20 20:32:05 -0500

    ...

--- index.js
+++ index.js
@@ -1,23 +1,10 @@
-const chromium = require("@sparticuz/chromium");
-const playwright = require("playwright-core");
 const app = require("express")();
 
 app.get("/", async (_, res) => {
   try {
-    const browser = await playwright.chromium.launch({
-      executablePath,
-      headless: true, // use this instead of using chromium.headless because it uses the new `headless: "new"` which will throw because playwright expects `headless: boolean`
-      args: chromium.args,
-    });
-
-    const page = await browser.newPage();
-    await page.goto("https://example.com");
-    const title = await page.title();
-    await browser.close();
-
-    res.send(title);
+    res.send("xxxx");
   } catch (error) {
-    res.send(error.message);
+    res.send("error");
   }
 });
 
