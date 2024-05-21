ce7ced91ec1d36557010b5acfbc317f1849d1db1
JosephVTX <jvcallupe69@gmail.com>
2024-05-20 20:27:33 -0500

    ...

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 const chromium = require("@sparticuz/chromium");
-const puppeteer = require("puppeteer-core");
+const playwright = require("playwright-core");
 const app = require("express")();
 
 app.get("/", async (_, res) => {
-  const browser = await puppeteer.launch({
-    args: chromium.args,
-    defaultViewport: chromium.defaultViewport,
-    executablePath: await chromium.executablePath(),
-    headless: chromium.headless,
-  });
+  try {
+    const browser = await playwright.chromium.launch({
+      executablePath,
+      headless: true, // use this instead of using chromium.headless because it uses the new `headless: "new"` which will throw because playwright expects `headless: boolean`
+      args: chromium.args,
+    });
 
-  const page = await browser.newPage();
-  await page.goto("https://example.com");
-  const title = await page.title();
-  await browser.close();
+    const page = await browser.newPage();
+    await page.goto("https://example.com");
+    const title = await page.title();
+    await browser.close();
 
-  res.send(title);
+    res.send(title);
+  } catch (error) {
+    res.send(error.message);
+  }
 });
 
 app.listen(3000, () => {
